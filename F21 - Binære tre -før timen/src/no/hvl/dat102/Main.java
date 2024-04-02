7c24509f443cc09b0263b092b204a7b904310ae3
soh1961 <soh@hvl.no>
2024-04-02 07:52:53 +0200

    Lagt til ein liten figur i main

--- F21 - Binære tre -før timen/src/no/hvl/dat102/Main.java
+++ F21 - Binære tre -før timen/src/no/hvl/dat102/Main.java
@@ -3,16 +3,16 @@ package no.hvl.dat102;
 import java.util.Iterator;
 
 public class Main {
- public static void main(String[] args) {
-	 BinaerTre<Integer> t1 = new BinaerTre<>(1);
-	 BinaerTre<Integer> t2 = new BinaerTre<>(7);
-	 BinaerTre<Integer> t3 = new BinaerTre<>(5, t1, t2);
-	 // t3.setTre(5, t1, t2);
-	 Iterator<Integer> it = t3.getInordenIterator();
-	 
-	 while (it.hasNext()) {
-		 System.out.println(it.next());
-	 }
-	 
- }
+	public static void main(String[] args) {
+		BinaerTre<Integer> t1 = new BinaerTre<>(1);
+		BinaerTre<Integer> t2 = new BinaerTre<>(7);
+		BinaerTre<Integer> t3 = new BinaerTre<>(5, t1, t2);
+
+		Iterator<Integer> it = t3.getInordenIterator();
+
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+
+	}
 }
